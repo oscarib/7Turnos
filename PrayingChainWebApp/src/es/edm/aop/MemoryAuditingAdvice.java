4d331822d77c12a017d2ec4df9e315c6d108b5ef
Oscar Ibáñez <oscar.ibafer@gmail.com>
2016-02-02 09:53:44 +0100

    Include Memory Auditing Advice
    - Adds around advice class
    - Removes memory mngmt. on IndexController
    - Add AspectJ jar support on pm.xml
    - Adds advice to config xml

---
+++ PrayingChainWebApp/src/es/edm/aop/MemoryAuditingAdvice.java
@@ -0,0 +1,58 @@
+package es.edm.aop;
+
+import java.text.DecimalFormat;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class MemoryAuditingAdvice{
+
+	public Object auditMemory(ProceedingJoinPoint method) throws Throwable {
+
+		//Now showing used Memory Before running the proxied method
+		Runtime runtime = Runtime.getRuntime();
+		double freeMemoryBefore = runtime.freeMemory();
+		double totalMemoryBefore = runtime.totalMemory();
+		double usedMemoryBefore = (totalMemoryBefore-freeMemoryBefore)/(1024*1024);
+		String usedMemoryBeforeSt = new DecimalFormat("#.##").format(usedMemoryBefore) + "Mb";
+		System.out.println("");
+		System.out.println("Memory Used before calling " + method.getSignature().getName() + ": " + usedMemoryBeforeSt);
+
+		try {
+
+			Object returnValue = method.proceed();
+
+			return returnValue;
+
+		} finally {
+
+			//Now showing used Memory After running the proxied method
+			double freeMemoryAfter = runtime.freeMemory();
+			double totalMemoryAfter = runtime.totalMemory();
+			double usedMemoryAfter = (totalMemoryAfter-freeMemoryAfter)/(1024*1024);
+			String usedMemoryAfterSt = new DecimalFormat("#.##").format(usedMemoryAfter) + "Mb";
+			System.out.println("Memory Used After calling " + method.getSignature().getName() + ": " + usedMemoryAfterSt);
+			
+			//Showing totao used memory by the proxied method
+			double usedMemory = (usedMemoryAfter-usedMemoryBefore)/(1024*1024);
+			String usedMemorySt = new DecimalFormat("#.##").format(usedMemory) + "Mb";
+			System.out.println("Total Memory Used by " + method.getSignature().getName() + ": " + usedMemorySt);
+
+			//Now calling the Garbage Collector
+			runtime.gc();
+			
+			//Now showing used Memory AfterGC running the Garbage collector
+			double freeMemoryAfterGC = runtime.freeMemory();
+			double totalMemoryAfterGC = runtime.totalMemory();
+			double usedMemoryAfterGC = (totalMemoryAfterGC-freeMemoryAfterGC)/(1024*1024);
+			String usedMemoryAfterGCSt = new DecimalFormat("#.##").format(usedMemoryAfterGC) + "Mb";
+			System.out.println("Memory Used AfterGC calling " + method.getSignature().getName() + ": " + usedMemoryAfterGCSt);
+		}
+	}
+	
+	public void writeMethod(JoinPoint method){
+		System.out.println("Se va a llamar a un método..." + method.getSignature().getName());
+		
+	}
+
+}
--- PrayingChainWebApp/src/es/edm/controllers/IndexController.java
+++ PrayingChainWebApp/src/es/edm/controllers/IndexController.java
@@ -1,7 +1,5 @@
 package es.edm.controllers;
 
-import java.text.DecimalFormat;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,14 +10,6 @@ public class IndexController {
 
 	@RequestMapping(path="/index", method=RequestMethod.GET)
 	public ModelAndView showBootstrap(){
-		Runtime runtime = Runtime.getRuntime();
-		double usedMemory = runtime.totalMemory()-runtime.freeMemory();
-		String usedMemoryString = new DecimalFormat("#.##").format(usedMemory/(1024*1024));
-		System.out.println("Used Memory before calling the Garbage Collector: " + usedMemoryString + "Mb");
-		runtime.gc();
-		usedMemory = runtime.totalMemory()-runtime.freeMemory();
-		usedMemoryString = new DecimalFormat("#.##").format(usedMemory/(1024*1024));
-		System.out.println("Used Memory before calling the Garbage Collector: " + usedMemoryString + "Mb");
 		return new ModelAndView("/web/index.jsp");
 	}
 }
