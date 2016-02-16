63ed43d1d8e68983338502432e303649d380daac
Oscar Ibáñez <oscar.ibafer@gmail.com>
2016-02-16 09:12:43 +0100

    Now the Controller do all the stuff
    In order to avoid hosted tomcat to give errors, we
    do now all the stuff from within the Controller,
    instead of passing the work to services classes.
    This approach avoids having to pass the WebRequest
    object to a third class, and we can now process
    the mailchimp request from within the own
    Controller, where all data is present. Then, the
    controller instantiate the email threaded
    instance, while Mailchimp has already been
    answered by the controller, and all data from the
    request has been properly injected into the sender
    without having to ask the WebRequest object.
    
    Changes:
    - Removes MailingListService
    - Removes MailingListRequestProcessor
    - Adds to MailingServiceIntegrationController all
      stuff needed to process the request, including
      thread declaration
    - Makes EmailSender to be a thread.

--- PrayingChainWebApp/src/es/edm/util/EmailSender.java
+++ PrayingChainWebApp/src/es/edm/util/EmailSender.java
@@ -1,5 +1,6 @@
 package es.edm.util;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.mail.internet.InternetAddress;
@@ -11,7 +12,7 @@ import org.apache.commons.mail.SimpleEmail;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmailSender {
+public class EmailSender extends Thread{
 	
 	String userName;
 	String userPassword;
@@ -19,6 +20,13 @@ public class EmailSender {
 	int smtpPort;
 	boolean ssl;
 	Email email;
+	String subject;
+	String message;
+	InternetAddress sender;
+	List<InternetAddress> recipients;
+	List<InternetAddress> recipientsCC;
+	List<InternetAddress> recipientsCCO;
+	Thread t;
 	
 	public EmailSender(String userName, String hostName, String password, int smtpPort, boolean ssl){
 		this.userName = userName;
@@ -26,28 +34,85 @@ public class EmailSender {
 		this.hostName = hostName;
 		this.smtpPort = smtpPort;
 		this.ssl = ssl;
+		this.email = new SimpleEmail();
+		t = new Thread(this);
 	}
 	
-	public void sendEmail(String subject, String message, 
-						  InternetAddress sender, 
-						  List<InternetAddress> recipients,
-						  List<InternetAddress> recipientsCC,
-						  List<InternetAddress> recipientsCCO) throws EmailException{
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		sendEmail();
+	}
+	
+	private void sendEmail(){
 
-		this.email = new SimpleEmail();
 		email.setHostName(hostName);
 		email.setSmtpPort(smtpPort);
 		email.setAuthenticator(new DefaultAuthenticator(userName, userPassword));
 		email.setSSLOnConnect(ssl);
-		setFrom(sender);
-		setRecipients(recipients);
-		setRecipientsCC(recipientsCC);
-		setRecipientsCCO(recipientsCCO);
-		email.setSubject(subject);
-		email.setMsg(message);
-		email.send();
+		try{
+			if (sender==null){
+				throw new RuntimeException("You should set the email sender by using setSender(InternetAddress sender)");
+			} else {
+				setFrom(sender);
+			}
+			if (recipients==null){
+				throw new RuntimeException("You should set at least one recepient for the email by using setRecipients(List<InternetAddress> recipìents)");
+			} else {
+				setRecipients(recipients);
+			}
+			if (recipientsCC!=null){
+				setRecipientsCC(recipientsCC);
+			}
+			if (recipientsCCO!=null){
+				setRecipientsCCO(recipientsCCO);
+			}
+			if (subject==null){
+				throw new RuntimeException("You should set the email subject by using setSubject(String subject)");
+			} else {
+				email.setSubject(subject);
+			}
+			if (message==null){
+				throw new RuntimeException("You should set the email message by using setMsg(String message)");
+			} else {
+				email.setMsg(message);
+			}
+			
+			email.send();
+			
+			//Including recipients in the log...
+			System.out.print(new Date() + ": An email was sucesfully sent to ");
+			for (InternetAddress nextRecp: recipients){
+				System.out.print(nextRecp.getAddress() + ", ");
+			}
+			System.out.println("");
+			
+		} catch (EmailException e){
+			System.out.println(new Date() + ": Something went really wrong while trying to send an email. Stack trace is:");
+			e.printStackTrace();
+		}
 	}
 	
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSender(InternetAddress sender) {
+		this.sender = sender;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	public void setFrom(InternetAddress sender) throws EmailException{
 		if (sender.getPersonal()!=null || !sender.getPersonal().equals("")){
 			email.setFrom(sender.getAddress(), sender.getPersonal());
@@ -58,6 +123,7 @@ public class EmailSender {
 	
 	public void setRecipients(List<InternetAddress> recipients) throws EmailException{
 		if (recipients!=null && recipients.size()>0){
+			this.recipients = recipients;
 			for (InternetAddress nextRecipient : recipients){
 				setTo(nextRecipient);
 			}
@@ -68,6 +134,7 @@ public class EmailSender {
 
 	public void setRecipientsCC(List<InternetAddress> recipientsCC) throws EmailException{
 		if (recipientsCC!=null && recipientsCC.size()>0){
+			this.recipientsCC = recipientsCC;
 			for (InternetAddress nextRecipient : recipientsCC){
 				setToCC(nextRecipient);
 			}
@@ -76,6 +143,7 @@ public class EmailSender {
 
 	public void setRecipientsCCO(List<InternetAddress> recipientsCCO) throws EmailException{
 		if (recipientsCCO!=null && recipientsCCO.size()>0){
+			this.recipientsCCO = recipientsCCO;
 			for (InternetAddress nextRecipient : recipientsCCO){
 				setToCCO(nextRecipient);
 			}
