3317f94337ce9c4d5340604042a83232ab0897e2
Oscar Ibáñez <oscar.ibafer@gmail.com>
2016-08-08 22:26:06 +0200

    Mejoras al dashboard (II)
    - Incluye animación angular
    - Incluye algunos widgets ya terminados
    - Incluye imágenes para los widgets

--- PrayingChainWebApp/WebContent/web/js/Controllers/dashboard.js
+++ PrayingChainWebApp/WebContent/web/js/Controllers/dashboard.js
@@ -1,7 +1,27 @@
-var app = angular.module("PrayingChain", []);
+var app = angular.module("PrayingChain", ['ngAnimate']);
 
 app.controller("dashboard", function($scope) {
 	var self = this;
     self.firstName = "John";
     self.lastName = "Doe";
+    self.hideActions = true;
+    self.hideListings = true;
+    
+    self.openActions = function(){
+    	if (self.hideActions){
+	        self.hideActions = false;
+	        self.hideListings = true;
+    	} else {
+	        self.hideActions = true;
+    	}
+    };
+
+    self.openListings = function(){
+    	if (self.hideListings){
+	        self.hideActions = true;
+	        self.hideListings = false;
+    	} else {
+    		self.hideListings = true;
+    	}
+    };
 });
