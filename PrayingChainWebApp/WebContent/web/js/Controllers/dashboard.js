89e344979a82ec8d17d3f7de35e075018b077931
Oscar Ibáñez <oscar.ibafer@gmail.com>
2016-08-09 17:18:02 +0200

    Instala Chart.js, y comienza el setting de los gráficos

--- PrayingChainWebApp/WebContent/web/js/Controllers/dashboard.js
+++ PrayingChainWebApp/WebContent/web/js/Controllers/dashboard.js
@@ -24,4 +24,41 @@ app.controller("dashboard", function($scope) {
     		self.hideListings = true;
     	}
     };
+    
+    var ctx = document.getElementById("myChart");
+    
+    var data = {
+        labels: ["Comprometidos", "No comprometidos"],
+        datasets: [{
+            data: [973, 116],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+    
+    var options = {
+    		scales: {
+    			yAxes: [{
+    				ticks: {
+    					beginAtZero:true
+    				}
+    			}]
+    		},
+    		legend: {
+    			display: false
+    		}
+    };
+    
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: options
+    });
 });
