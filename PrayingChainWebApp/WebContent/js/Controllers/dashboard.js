e0e29488aa092c197875cf4bcb29549971c28648
Oscar Ibáñez <oscar.ibafer@gmail.com>
2016-08-10 17:36:05 +0200

    Renombre la variable hideListings por hideLists

--- PrayingChainWebApp/WebContent/js/Controllers/dashboard.js
+++ PrayingChainWebApp/WebContent/js/Controllers/dashboard.js
@@ -5,52 +5,59 @@ PrayingChain.controller("dashboard", ['chartService', 'prayerServices', function
     self.firstName = "John";
     self.lastName = "Doe";
     self.hideActions = true;
-    self.hideListings = true;
+    self.hideLists = true;
     
     self.openActions = function(){
     	if (self.hideActions){
 	        self.hideActions = false;
-	        self.hideListings = true;
+	        self.hideLists = true;
     	} else {
 	        self.hideActions = true;
     	}
     };
 
-    self.openListings = function(){
-    	if (self.hideListings){
+    self.openLists = function(){
+    	if (self.hideLists){
 	        self.hideActions = true;
-	        self.hideListings = false;
+	        self.hideLists = false;
     	} else {
-    		self.hideListings = true;
+    		self.hideLists = true;
     	}
     };
-
-    //Committed / Non committed pie chart
-    var labels= ["Comprometidos", "No Comprometidos"];
-    var pieChartData = [973,116];
-    var borderColor = ['rgba(54, 162, 235, 0.2)','rgba(255, 99, 132, 0.2)'];
-    var backgroundColor = ['rgba(54, 162, 235, 1)','rgba(255,99,132,1)'];
-    chartService.setPieChart("chartCommitted", labels, pieChartData, backgroundColor, borderColor);
     
-    //Coverage pie chart
-    var labels= ["Cubiertos", "Vacíos"];
-    var pieChartData = [1680,1];
-    var borderColor = ['rgba(54, 162, 235, 0.2)','rgba(255, 99, 132, 0.2)'];
-    var backgroundColor = ['rgba(54, 162, 235, 1)','rgba(255,99,132,1)'];
-    chartService.setPieChart("chartCoverage", labels, pieChartData, backgroundColor, borderColor);
+    loadCharts();
 
-    //availables Vs Total pie chart
-    var labels= ["Totales", "Disponibles"];
-    var pieChartData = [1680,715];
-    var borderColor = ['rgba(80, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'];
-    var backgroundColor = ['rgba(80,99,132,1)','rgba(54, 162, 235, 1)'];
-    chartService.setPieChart("availablePieChart", labels, pieChartData, backgroundColor, borderColor);
     
-    //Redundancy: overoccupied
-    var labels= ["Normales	", "Sobresaturados"];
-    var pieChartData = [100,13];
-    var borderColor = ['rgba(54, 162, 235, 0.2)','rgba(255, 99, 132, 0.2)'];
-    var backgroundColor = ['rgba(54, 162, 235, 1)','rgba(255,99,132,1)'];
-    chartService.setPieChart("redundancePieChart", labels, pieChartData, backgroundColor, borderColor);
-    redundancePieChart
+//FUNCIONES PRIVADAS -->
+    function loadCharts(){
+        //Committed / Non committed pie chart
+        var labels= ["Comprometidos", "No Comprometidos"];
+        var pieChartData = [973,116];
+        var borderColor = ['rgba(54, 162, 235, 0.2)','rgba(255, 99, 132, 0.2)'];
+        var backgroundColor = ['rgba(54, 162, 235, 1)','rgba(255,99,132,1)'];
+        chartService.setPieChart("chartCommitted", labels, pieChartData, backgroundColor, borderColor);
+        
+        //Coverage pie chart
+        var labels= ["Cubiertos", "Vacíos"];
+        var pieChartData = [1680,1];
+        var borderColor = ['rgba(54, 162, 235, 0.2)','rgba(255, 99, 132, 0.2)'];
+        var backgroundColor = ['rgba(54, 162, 235, 1)','rgba(255,99,132,1)'];
+        chartService.setPieChart("chartCoverage", labels, pieChartData, backgroundColor, borderColor);
+
+        //availables Vs Total pie chart
+        var labels= ["Totales", "Disponibles"];
+        var pieChartData = [1680,715];
+        var borderColor = ['rgba(80, 190, 132, 0.2)', 'rgba(13, 180, 220, 0.2)'];
+        var backgroundColor = ['rgba(80,190,132,1)','rgba(13, 180, 220, 1)'];
+        chartService.setPieChart("availablePieChart", labels, pieChartData, backgroundColor, borderColor);
+        
+        //Redundancy: overoccupied
+        var labels= ["Normales	", "Sobresaturados"];
+        var pieChartData = [100,13];
+        var borderColor = ['rgba(54, 162, 235, 0.2)','rgba(255, 99, 132, 0.2)'];
+        var backgroundColor = ['rgba(54, 162, 235, 1)','rgba(255,99,132,1)'];
+        chartService.setPieChart("redundancePieChart", labels, pieChartData, backgroundColor, borderColor);
+        redundancePieChart    		
+    };
+//<-- Fin de FUNCIONES PRIVADAS
 }]);
