b8be1ddc088ac43bbc39dfce53c45b128bdcdfa6
Oscar Ibáñez <oscar.ibafer@gmail.com>
2016-11-02 22:22:41 +0100

    Vuelve a incorporar la cascada en el mapeo a prayer de turns

--- PrayingChainWebApp/src/es/edm/domain/entity/PrayerEntity.java
+++ PrayingChainWebApp/src/es/edm/domain/entity/PrayerEntity.java
@@ -50,7 +50,7 @@ public class PrayerEntity {
 	@Column
 	private String pseudonym;
 	
-    @OneToMany(mappedBy="prayer")
+    @OneToMany(mappedBy="prayer", cascade = CascadeType.ALL)
     @Fetch(FetchMode.JOIN)
 	@JsonBackReference //Para evitar que la serializaci�n a JSON entre en un bucle infinito
 	private List<TurnEntity> turns;
