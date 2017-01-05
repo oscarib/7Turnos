a74c97c61ec80c49c426ff18bffe93d8935008c5
Oscar Ibáñez <oscar.ibafer@gmail.com>
2017-01-05 15:31:56 +0100

    Corrige Pais en create prayer (Fase I)

--- 7Turnos/WebContent/js/controllers/newPrayer.js
+++ 7Turnos/WebContent/js/controllers/newPrayer.js
@@ -62,7 +62,7 @@ PrayingChain.controller("newPrayer", ['$scope','$rootScope','prayerServices','$l
 		self.visibilidades = [$rootScope.labels.hidden,$rootScope.labels.publico];
 		self.daysOfWeek = [$rootScope.labels.monday, $rootScope.labels.tuesday, $rootScope.labels.wednesday,
 		                   $rootScope.labels.thursday, $rootScope.labels.friday, $rootScope.labels.saturday, $rootScope.labels.sunday];
-		self.paises = [$rootScope.labels.country, $rootScope.labels.otherCountry];
+		self.paises = [$rootScope.labels.ownCountry, $rootScope.labels.otherCountry];
 		self.turnos = ["00:00am","00:30am","01:00am","01:30am","02:00am","02:30am","03:00am","03:30am","04:00am","04:30am",
 		               "05:00am","05:30am","06:00am","06:30am","07:00am","07:30am","08:00am","08:30am","10:00am","10:30am",
 		               "11:00am","11:30am","12:00pm","12:30pm","13:00pm","13:30pm","14:00pm","14:30pm","15:00pm","15:30pm",
