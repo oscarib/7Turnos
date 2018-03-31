85d255915514ba9d53f0a219776885a03c467e0b
Oscar Ib <oscar.ibafer@gmail.com>
2018-03-31 19:59:50 +0200

    Adds support for service injection on Quartz Jobs

--- 7Turnos/src/es/edm/quartz/EmailSchedulerJob.java
+++ 7Turnos/src/es/edm/quartz/EmailSchedulerJob.java
@@ -1,20 +1,25 @@
 package es.edm.quartz;
 
-import es.edm.dao.impl.OthersDao;
+import es.edm.services.Impl.PrayerService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import java.util.Date;
+import javax.inject.Inject;
 
 public class EmailSchedulerJob extends QuartzJobBean {
 
     private final static Logger logger = LoggerFactory.getLogger(EmailSchedulerJob.class);
 
+    @Inject
+    PrayerService prayerService;
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        logger.info("El EmailScheduler se ha ejecutado a las " + new Date());
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+        logger.info("Se ha ejecutado correctamente el EmailScheduler");
     }
 }
